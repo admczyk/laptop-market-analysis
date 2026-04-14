554db16b757183d96553ac2ac1bca2a52c9df9fe
bezwymowki <klaudia.adamczyk707@gmail.com>
2026-04-15 00:42:43 +0200

    Made final version of main scraping logic

--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,15 @@ from selenium.webdriver.common.by import By
 import time
 import json
 
+def check_if_store_exists(stores, domain):
+    if len(stores) == 0:
+        return True
+    for store in stores:
+        if store["domain"] == domain:
+            return False
+    return True
+
+
 driver = webdriver.Firefox()
 
 driver.get("https://www.ceneo.pl/Laptopy")
@@ -14,9 +23,8 @@ cookie.click()
 
 time.sleep(3)
 
-# potencjalnie wez to zmien pozniej tak, aby pobieralo tylko linki / jeden element 
-# z linkiem zamiast 5 do tego samego produktu
 next_btn = True
+tm = 4
 
 product_id = 1
 store_id = 1
@@ -31,12 +39,77 @@ while next_btn:
     laptops = driver.find_elements(By.CLASS_NAME, "go-to-product")
     links = [l.get_attribute("href") for l in laptops][::5]
 
-    print(links)
-    #later change this for loop, so it iterates through len(links)
-    for i in range(1): #range(len(links)):
+    for i in range(len(links)):
         driver.get(links[i])
         
-        time.sleep(3)
+        time.sleep(tm)
+
+        el = driver.find_elements(By.ID, "notifications-toast")
+        popup_visible = any(e.is_displayed() for e in el)
+
+        if popup_visible:
+            decline = driver.find_element(By.CLASS_NAME, "button.js_notification_no")
+            decline.click()
+            tm = 1
+
+        # store_product_offers = []
+
+        all_stores = driver.find_elements(By.CLASS_NAME, "product-offer__store-with-product")
+        for store in all_stores:
+            store_logo = store.find_elements(By.CLASS_NAME, "store-logo")
+
+            if not store_logo:
+                continue
+
+            domain = store_logo[0].find_element(By.CSS_SELECTOR, "img").get_attribute("alt")
+
+            # will be using it later, when writing logic for calculating mean product rating
+            # store_offer_link = store_logo[0].get_attribute("href")
+            # store_product_offers.append(store_offer_link)
+
+            price = store.find_element(By.CLASS_NAME, "price").get_attribute("textContent")
+
+            # later make it separate function
+            if check_if_store_exists(stores, domain): 
+                dot_idx = domain.find(".")
+                store_name = domain[:dot_idx]
+
+                store_rating = store.find_elements(By.CLASS_NAME, "screen-reader-text")
+                if not store_rating:
+                    store_rating = 0
+                    store_reviews_count = 0
+                else:
+                    store_rating = store_rating[0].get_attribute("textContent")
+                    comma_idx = store_rating.find(",")
+                    store_rating = store_rating[comma_idx - 1:comma_idx + 2]
+
+                    store_reviews_count = store.find_element(By.CLASS_NAME, "link").get_attribute("textContent")
+                    store_reviews_count = store_reviews_count[:-7]
+
+                store_data = {
+                    "id": store_id,
+                    "name": store_name,
+                    "domain": domain,
+                    "rating_score": store_rating,
+                    "rating_count": store_reviews_count
+                }
+
+                stores.append(store_data)
+                store_id += 1
+
+            s = [s["id"] for s in stores if s["domain"] == domain]
+            print(s)
+            
+            offer_data = {
+                "product_id": product_id,
+                "store_id": s[0],
+                "price": price,
+                "currency": "PLN"
+            }
+
+            offers.append(offer_data)
+
+        time.sleep(tm)
 
         button = driver.find_element(By.CSS_SELECTOR, "li.page-tab.spec")
         button.click()
@@ -80,64 +153,18 @@ while next_btn:
                 product_data[product_spec_headers[i].text] = sub_attributes
 
         products.append(product_data)
-        
-        store_product_offers = []
-
-        all_stores = driver.find_elements(By.CLASS_NAME, "product-offer__store-with-product")
-        for store in all_stores:
-            domain = store.find_element(By.CSS_SELECTOR, "img").get_attribute("alt")
-
-            # later will be used to calculate mean rating of product
-            store_offer_link = store.find_element(By.CLASS_NAME, "store-logo").get_attribute("href")
-            store_product_offers.append(store_offer_link)
-
-            price = store.find_element(By.CLASS_NAME, "price").get_attribute("textContent")
-
-            # later make it separate function
-            for s in stores:
-                if s["domain"] == domain:
-                   pass
-            else: 
-                dot_idx = domain.find(".")
-                store_name = domain[:dot_idx]
-
-                store_rating = store.find_element(By.CLASS_NAME, "screen-reader-text").get_attribute("textContent")
-                comma_idx = store_rating.find(",")
-                store_rating = store_rating[comma_idx - 1:comma_idx + 2]
-
-                store_reviews_count = store.find_element(By.CLASS_NAME, "link").get_attribute("textContent")
-                store_reviews_count = store_reviews_count[:-7]
-
-                store_data = {
-                    "id": store_id,
-                    "name": store_name,
-                    "domain": domain,
-                    "rating_score": store_rating,
-                    "rating_count": store_reviews_count
-                }
-
-                stores.append(store_data)
-                store_id += 1
-
-            s = [s["id"] for s in stores if s["domain"] == domain]
-            
-            offer_data = {
-                "product_id": product_id,
-                "store_id": s[0],
-                "price": price,
-                "currency": "PLN"
-            }
-
-            offers.append(offer_data)
     
         product_id += 1
-        time.sleep(3)
+        time.sleep(2)
     driver.get(next_page)
     next_btn = False
 
 print(stores)
 print(offers)
 print(products)
+
+
+
 
 with open("./data/raw/products_data.JSON", "w", encoding="utf-8") as file:
     json.dump(products, file, ensure_ascii=False, indent=4)
